c3ba1878b772ed0d3decaadc6173cc9034cd1ac7
asha192sf <112470470+asha192sf@users.noreply.github.com>
2022-08-30 18:06:07 +1000

    Update streamlit_app.py
    Better menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,7 @@ import streamlit
 streamlit.title('This is my first App')
 
 streamlit.header('menu for the D-day')
-streamlit.text('Dosa')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
